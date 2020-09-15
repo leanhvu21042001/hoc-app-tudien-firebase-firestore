0567abde3e23ff8f55ead25735d9c3563357cde9
Lê Anh Vũ <leanhvu>
2020-09-15 10:33:47 +0700

    updating

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,8 @@
+/**
+ * Author: Lê Anh Vũ
+ * firebase-app, firebase-store
+ * 15/09/2020
+ */
 const listGroup = document.querySelector('.word-list');
 const formAddNewWord = document.querySelector('#addNewWord');
 function renderListWords(doc) {
@@ -7,9 +12,11 @@ function renderListWords(doc) {
   let cross = document.createElement('div');
 
   li.setAttribute('data-id', doc.id);
-  li.setAttribute('class','word');
+  li.setAttribute('class', 'word-mean');
   word.textContent = doc.data().word;
+  word.setAttribute('class', 'word');
   mean.textContent = doc.data().mean;
+  mean.setAttribute('class', 'mean');
   cross.textContent = 'x';
 
   li.appendChild(word);
@@ -25,29 +32,6 @@ function renderListWords(doc) {
     console.log(id);
     db.collection('EnglishWords').doc(id).delete();
   });
-  word.addEventListener('click', function (e) {
-    e.preventDefault();
-    let id = e.target.parentElement.getAttribute('data-id');
-    const flag = confirm('Bạn mốn thay đổi không ?');
-    if (flag) {
-      let newWord = '';
-      let newMean = '';
-      do {
-        newWord = prompt('Nhập từ mới: ');
-      } while (newWord.length <= 0);
-      do {
-        newMean = prompt('Nhập nghĩa từ mới: ');
-      } while (newMean.length <= 0);
-      // console.log(newWord);
-      // console.log(newMean);
-
-      db.collection('EnglishWords').doc(id).update({
-        word: newWord,
-        mean: newMean
-      });
-      location.reload();
-    }
-  });
 }
 
 // saving data (saving new word)
@@ -62,11 +46,12 @@ formAddNewWord.addEventListener('submit', function (event) {
   formAddNewWord.word.value = '';
   formAddNewWord.mean.value = '';
 });
+
 // real-time listener
 db.collection('EnglishWords').orderBy('word').onSnapshot(snapshot => {
+
   let changes = snapshot.docChanges();
-  let arr = new Array(changes);
-  console.log(changes)
+  // xóa sẽ xóa cái hiện tại nếu không sẽ tải lại danh sách
   changes.forEach(change => {
     if (change.type === 'added') {
       renderListWords(change.doc);
@@ -74,5 +59,42 @@ db.collection('EnglishWords').orderBy('word').onSnapshot(snapshot => {
       let li = listGroup.querySelector("[data-id=" + change.doc.id + "]");
       listGroup.removeChild(li);
     }
+  });
+
+  // change word
+  const wordMean = document.querySelectorAll('.word-mean');
+  wordMean.forEach(element => {
+    element.addEventListener('click', function (e) {
+      // khai báo biến
+      const flag = confirm('Bạn mốn thay đổi không ?');
+      let id = e.target.parentElement.getAttribute('data-id');
+      let newWord = '';
+      let newMean = '';
+      // kiểm tra xác nhận thay đổi
+      if (flag) {
+        do {
+          newWord = prompt('Nhập từ mới: ');
+        } while (newWord.length <= 0);
+        do {
+          newMean = prompt('Nhập nghĩa từ mới: ');
+        } while (newMean.length <= 0);
+        // hiển thị dữ liệu mới
+        console.log('New Word: ', newWord);
+        console.log('Mean Word: ', newMean);
+        // dữ liệu hiện tại
+        console.log(this.children[0].textContent);
+        console.log(this.children[1].textContent);
+        console.log(id);
+        // hiển thị hiện tại
+        this.children[0].textContent = newWord;
+        this.children[1].textContent = newMean;
+        // cập nhập db
+        db.collection('EnglishWords').doc(id).update({
+          word: newWord,
+          mean: newMean
+        });
+      }
+      e.preventDefault();
+    });
   });
 });
