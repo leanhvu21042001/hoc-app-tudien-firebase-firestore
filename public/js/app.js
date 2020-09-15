2fe033134452b6f8d7e759e748cd29ce6c8d678f
Lê Anh Vũ <leanhvu>
2020-09-15 07:51:07 +0700

    app tudien

---
+++ public/js/app.js
@@ -0,0 +1,72 @@
+const listGroup = document.querySelector('.word-list');
+const formAddNewWord = document.querySelector('#addNewWord');
+function renderListWords(doc) {
+  let li = document.createElement('li');
+  let word = document.createElement('span');
+  let mean = document.createElement('span');
+  let cross = document.createElement('div');
+
+  li.setAttribute('data-id', doc.id);
+  li.setAttribute('class','word');
+  word.textContent = doc.data().word;
+  mean.textContent = doc.data().mean;
+  cross.textContent = 'x';
+
+  li.appendChild(word);
+  li.appendChild(mean);
+  li.appendChild(cross);
+
+  listGroup.appendChild(li);
+
+  //deleting data
+  cross.addEventListener('click', function (e) {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('data-id');
+    console.log(id);
+    db.collection('EnglishWords').doc(id).delete();
+  });
+  word.addEventListener('click', function (e) {
+    e.preventDefault();
+    let id = e.target.parentElement.getAttribute('data-id');
+    console.log(id);
+    console.log(e.target.textContent);
+    const flag = confirm('ban muon thay doi khong');
+    if (flag) {
+      const newWord = prompt('Nhap tu moi: ');
+      const newMean = prompt('Nhap nghia cua tu: ');
+      console.log(newWord);
+      console.log(newMean);
+
+      db.collection('EnglishWords').doc(id).update({
+        word: newWord,
+        mean: newMean
+      });
+      location.reload();
+    }
+  });
+}
+
+// saving data (saving new word)
+formAddNewWord.addEventListener('submit', function (event) {
+  event.preventDefault();
+  db.collection('EnglishWords').add({
+    word: formAddNewWord.word.value,
+    mean: formAddNewWord.mean.value
+  });
+  console.log(formAddNewWord.word.value);
+  console.log(formAddNewWord.mean.value);
+  formAddNewWord.word.value = '';
+  formAddNewWord.mean.value = '';
+});
+// real-time listener
+db.collection('EnglishWords').onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  changes.forEach(change => {
+    if (change.type === 'added') {
+      renderListWords(change.doc);
+    } else if (change.type === 'removed') {
+      let li = listGroup.querySelector("[data-id=" + change.doc.id + "]");
+      listGroup.removeChild(li);
+    }
+  });
+});
---
+++ public/js/firebase-config.js
@@ -0,0 +1,13 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD-yIaO80xFtOYjtQm7U5DJ3qBupjk1FA4",
+    authDomain: "app-tudien-firebase-fire-9b99e.firebaseapp.com",
+    databaseURL: "https://app-tudien-firebase-fire-9b99e.firebaseio.com",
+    projectId: "app-tudien-firebase-fire-9b99e",
+    storageBucket: "app-tudien-firebase-fire-9b99e.appspot.com",
+    messagingSenderId: "1094004567141",
+    appId: "1:1094004567141:web:2548f49b385c08a9fe2a8c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
